c61e485b6222089a518fe992e42cd58ccc3bf1b4
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-01-12 14:08:49 -0700

    restarted and added new css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,9 @@
-// import Design from './components/Design';
 import Search from './components/Search';
 
 function App() {
   return (
     <div className='App App-header'>
       <Search />
-      {/* <Design /> */}
     </div>
   );
 }
