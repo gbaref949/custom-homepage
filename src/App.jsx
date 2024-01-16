5df727a97ef99903e5eb62a23bf9c6d70788851d
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-01-16 09:09:50 -0700

    created barebones for menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Search from './components/Search';
+import Other from './components/Other';
 
 function App() {
   return (
     <div className='body'>
       <Search />
+      <Other />
     </div>
   );
 }
---
+++ src/components/Other.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Other = () => {
+  return (
+    <div className='menu'>
+        <section className='item'>
+            <h1>HI</h1>
+        </section>
+    </div>
+  )
+}
+
+export default Other
