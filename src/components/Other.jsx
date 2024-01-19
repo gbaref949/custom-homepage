8c52de28d38d009707c1acaf476064cf368e2ed4
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-01-19 05:04:42 -0700

    new accordion

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -1,26 +1,31 @@
-import Accordion from 'react-bootstrap/Accordion';
 
 const Other = () => {
   return (
-    <div className='menu'>
-      <section className='item'>
-        <Accordion>
-          <Accordion.Item eventKey='0'>
-            <Accordion.Header>Accordion Item #1</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey='1'>
-            <Accordion.Header>Accordion Item #2</Accordion.Header>
-            <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
-      </section>
+    <div className="div">
+      <details open>
+        <summary>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In, autem!</summary>
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio facere, nostrum unde iste beatae rerum reprehenderit hic nihil culpa obcaecati. Ducimus eos illum blanditiis enim tempore dicta veritatis cum facere labore adipisci porro atque iusto voluptatem fugit, nam laborum in unde officiis libero quae eius numquam molestias quos voluptates! Accusamus!
+        </div>
+      </details>
+      <details>
+        <summary>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, laudantium.</summary>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi accusantium in qui inventore, aliquid eius harum fugiat magni. Tenetur, ipsum. Culpa porro placeat dolor animi maiores in, repellendus quaerat obcaecati laboriosam accusamus dolorem mollitia cupiditate nam modi minima similique debitis ipsam repudiandae ullam consectetur magnam. Illum culpa perspiciatis quae pariatur?
+        </div>
+      </details>
+      <details>
+        <summary>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quod, voluptatibus!</summary>
+        <div>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nulla, quo? Harum tempora ipsa ut hic. Aut hic temporibus qui atque, fugiat dolor repudiandae placeat recusandae dicta pariatur officia quisquam, dolore fugit provident dolores excepturi deleniti odio neque totam? Ratione sed illo dolores blanditiis voluptas nesciunt debitis dolore aut cupiditate tempora?
+        </div>
+      </details>
+      <details>
+        <summary>Lorem ipsum dolor sit, amet consectetur adipisicing.</summary>
+        <div>
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex hic facere, neque veniam aspernatur et deserunt eveniet odit, blanditiis architecto in est eum, laudantium reprehenderit! Ipsa nesciunt ducimus, quo quae labore error exercitationem assumenda reiciendis.
+        </div>
+      </details>
     </div>
   );
 };
