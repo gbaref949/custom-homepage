063d4174d826b639d8fe90806b63ea8b9c3511d3
Georgiana Barefield <110576775+gbaref949@users.noreply.github.com>
2024-01-19 04:55:22 -0700

    added changes to pckg json

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import Accordion from 'react-bootstrap/Accordion';
 
 const Other = () => {
   return (
     <div className='menu'>
-        <section className='item'>
-            <h1>HI</h1>
-        </section>
+      <section className='item'>
+        <Accordion>
+          <Accordion.Item eventKey='0'>
+            <Accordion.Header>Accordion Item #1</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Accordion.Body>
+          </Accordion.Item>
+          <Accordion.Item eventKey='1'>
+            <Accordion.Header>Accordion Item #2</Accordion.Header>
+            <Accordion.Body>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            </Accordion.Body>
+          </Accordion.Item>
+        </Accordion>
+      </section>
     </div>
-  )
-}
+  );
+};
 
-export default Other
+export default Other;
